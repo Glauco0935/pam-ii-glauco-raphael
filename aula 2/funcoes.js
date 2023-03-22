c7c0dba3ae78a3764ac8c140390c2e5d0cf6b470
Glauco0935 <aktglauco0935@gmail.com>
2023-03-22 08:28:47 -0300

    Aula sobre variaveis e tipos

---
+++ aula 2/funcoes.js
@@ -0,0 +1,29 @@
+//let num1 = 2
+//const num2 = 3
+//var msg = "Olá amigos! Esse é o número:"
+
+
+//console.log(msg + num1)
+//console.log(num1 + num2)
+//console.log(typeof num1)
+//console.log(typeof msg)
+
+
+// JSON - JavaScript Object Notation
+
+//let usuario = {
+ //   nome: "João",
+   // idade: 35,
+    //signo: "calvo",
+    //calvo: true
+//}
+
+//console.log (typeof usuario)
+//console.log ("nome:", usuario.nome, "\n idade:", usuario.idade)
+//console.log (typeof usuario.calvo)
+
+const nome = ["João, Oãoj"]
+
+console.log (nomes)
+console.log (typeof nomes)
+console.log (nomes[0])
